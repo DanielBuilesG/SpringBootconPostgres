30047d0831a58d485df1e69e401532249b8722b1
DanielBuilesG <daniel.builes99@gmail.com>
2023-09-22 19:29:02 -0500

    first commit

---
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,36 @@
+package com.example.demo.controllers;
+
+import com.example.demo.dto.UserDto;
+import com.example.demo.entity.User;
+import com.example.demo.exceptions.UserNotFoundException;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/v1/user")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(@Autowired UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping
+    User createUser(@RequestBody UserDto userDto) {
+        User user = new User(userDto);
+        return userRepository.save(user);
+    }
+
+    @GetMapping("/{id}")
+    User findById(@PathVariable Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isPresent())
+            return optionalUser.get();
+        else throw new UserNotFoundException();
+    }
+
+}
